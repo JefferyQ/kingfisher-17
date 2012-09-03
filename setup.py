5f780ca1be893f41cb71eb2352e47ae4829dcd84
Alex Lee <alex@thirdbeat.com>
2012-09-03 23:53:59 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='kingfisher',
+    version='0.1',
+    description='A DNS server',
+    long_description='A DNS server',
+    author='Alex Lee',
+    author_email='kingfisher@thirdbeat.com',
+    maintainer='Alex Lee',
+    maintainer_email='kingfisher@thirdbeat.com',
+    url='http://kingfisher.thirdbeat.com',
+    classifiers=[
+    ],
+    packages=['kingfisher'],
+)
+
